a6cd7456418a1b361306fa61a73a2b0a0621920a
Dang Duy Lan <ddlan99tn@gmail.com>
2023-04-03 20:53:37 +0700

    some updates for menu

--- Caro/Source.cpp
+++ Caro/Source.cpp
@@ -19,11 +19,8 @@ int main() {
 	//LoadMenu(_A, _TURN, _COMMAND, _X, _Y, cX, cY, cntX, cntO, cntWinO, cntLoseO, cntDraw, saveTurn);
 	//string tmp = "test2.txt";
 	//SaveData(_A, _TURN, _COMMAND, _X, _Y, cX, cY, cntX, cntO, cntWinO, cntLoseO, cntDraw, saveTurn, tmp);
-	//MainMenu();
-	//_getch();
 	while (true) {
 		MainMenu(_A, _TURN, _COMMAND, _X, _Y, cX, cY, cntX, cntO, cntWinO, cntLoseO, cntDraw, saveTurn);
 	}
-	//StartGame(_A, _TURN, _COMMAND, _X, _Y, cX, cY, cntX, cntO, cntWinO, cntLoseO, cntDraw, saveTurn);
 	return 0;
 }
